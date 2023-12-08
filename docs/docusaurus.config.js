c25e6c1deefd34984b6e135f9f5a859e8bfc2b51
Reid Levesque <reidlevesque@users.noreply.github.com>
2023-12-08 15:55:02 -0500

    Add GH action to build and deploy docs on push to main

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -12,11 +12,8 @@ const config = {
   tagline: "OpenAssistants",
   favicon: "img/definitive.ico",
 
-  // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  url: "https://definitive-io.github.io",
+  baseUrl: "/openassistants/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
