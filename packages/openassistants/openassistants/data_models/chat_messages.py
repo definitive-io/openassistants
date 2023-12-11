5f94fc3e74ad55e438c5b613c10abb7d7afd3631
Jordan Wu <101218661+jordan-definitive@users.noreply.github.com>
2023-12-11 15:27:37 -0800

    clean chat history representation (#29)

--- packages/openassistants/openassistants/data_models/chat_messages.py
+++ packages/openassistants/openassistants/data_models/chat_messages.py
@@ -46,8 +46,22 @@ class OpasAssistantMessage(BaseModel):
     )
 
 
-def _render_df_for_llm(df: pd.DataFrame) -> str:
-    return df.to_csv(index=False, date_format="iso")
+class OpasFunctionMessage(BaseModel):
+    role: Literal["function"] = "function"
+    name: str
+    outputs: Annotated[
+        List[FunctionOutput],
+        Field(
+            description="the outputs of the function. "
+            "can only be certain types so the client knows how to display it"
+        ),
+    ]
+
+
+OpasMessage = Annotated[
+    OpasUserMessage | OpasAssistantMessage | OpasFunctionMessage,
+    Field(json_schema_extra={"descriminator": "role"}),
+]
 
 
 def ensure_alternating(chat_history: List[BaseMessage]) -> List[BaseMessage]:
@@ -68,66 +82,3 @@ def ensure_alternating(chat_history: List[BaseMessage]) -> List[BaseMessage]:
             fixed.append(message)
 
     return fixed
-
-
-def opas_to_lc(chat_history, skip_assistant_messages=False) -> List[BaseMessage]:
-    lc_messages: List[BaseMessage] = []
-
-    for message in chat_history:
-        if isinstance(message, OpasUserMessage) and message.input_response is None:
-            lc_messages.append(HumanMessage(content=message.content))
-        if (
-            isinstance(message, OpasAssistantMessage)
-            and message.function_call is not None
-        ):
-            fname = message.function_call.name
-            fargs = message.function_call.arguments
-            lc_messages.append(
-                HumanMessage(
-                    content=f"""\
-To answer the previous question, I called a function: {fname}({fargs})
-"""
-                )
-            )
-        if (
-            isinstance(message, OpasAssistantMessage)
-            and message.function_call is None
-            and skip_assistant_messages is False
-        ):
-            lc_messages.append(AIMessage(content=message.content))
-        if isinstance(message, OpasFunctionMessage):
-            function_message_summary = ""
-            for output in message.outputs:
-                if isinstance(output, DataFrameOutput):
-                    function_message_summary += f"""\
-The following table answers the previous question:
-{_render_df_for_llm(output.dataframe.to_pd())}
-"""
-                elif isinstance(output, TextOutput):
-                    function_message_summary += f"""\
-Insight for the previous question based on the data:
-{output.text}
-"""
-            lc_messages.append(HumanMessage(content=function_message_summary))
-
-    ensure_alternating(lc_messages)
-
-    return lc_messages
-
-
-class OpasFunctionMessage(BaseModel):
-    role: Literal["function"] = "function"
-    name: str
-    outputs: Annotated[
-        List[FunctionOutput],
-        Field(
-            description="the outputs of the function. "
-            "can only be certain types so the client knows how to display it"
-        ),
-    ]
-
-
-OpasMessage = Annotated[
-    OpasUserMessage | OpasAssistantMessage | OpasFunctionMessage,
-    Field(json_schema_extra={"descriminator": "role"}),
-]
--- packages/openassistants/openassistants/utils/yaml.py
+++ packages/openassistants/openassistants/utils/yaml.py
@@ -1,12 +1,40 @@
-import yaml
-
+import copy
+from io import StringIO
 from pathlib import Path
-from typing import Optional, TextIO
+from typing import Iterator, TextIO
+
+from ruamel.yaml import YAML
+from ruamel.yaml.scalarstring import walk_tree
 
 
-def dump(data: dict, f=None) -> Optional[str]:
-    return yaml.dump(data, f)
-    
+def _convert_literal_scalarstrings(data: dict | list[dict]):
+    data_copy = copy.deepcopy(data)
+    walk_tree(data_copy)
+    return data_copy
 
-def load(data_raw: str | TextIO | Path):
-     return yaml.safe_load(data_raw)  # type: ignore
+
+def dump_all(data: list[dict], f: Path | TextIO) -> None:
+    data_copy = _convert_literal_scalarstrings(data)
+    YAML().dump_all(data_copy, f)
+
+
+def dumps_all(data: list[dict]) -> str:
+    sio = StringIO()
+    dump_all(data, sio)
+    return sio.getvalue()
+
+
+def dump(data: dict, f: Path | TextIO) -> None:
+    dump_all([data], f)
+
+
+def dumps(data: dict) -> str:
+    return dumps_all([data])
+
+
+def load(data_raw: str | TextIO | Path) -> dict:
+    return YAML().load(data_raw)
+
+
+def load_all(data_raw: str | TextIO | Path) -> Iterator[dict]:
+    return YAML().load_all(data_raw)
