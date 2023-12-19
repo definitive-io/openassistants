14e2e4b7c6ffcfabe68f90713309d32b664ec26f
Cole McCracken <colemccracken@gmail.com>
2023-12-19 13:44:19 -0500

    hardcode backend

--- examples/next/src/app/page.tsx
+++ examples/next/src/app/page.tsx
@@ -8,10 +8,15 @@ export default function Home() {
       <div className="relative flex w-full h-full overflow-hidden">
         <div className="group w-full pl-0 animate-in duration-300 ease-in-out peer-[[data-state=open]]:lg:pl-[250px] peer-[[data-state=open]]:xl:pl-[300px]">
           <OpenAssistantsChat
+<<<<<<< Updated upstream
             api={`${
               process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000'
             }/v1alpha/assistants/hooli/chat`}
             name="Chevrolet of Watsonville Chat Team"
+=======
+            api={`https://openassistants-fast-api-server-pr-56.onrender.com/v1alpha/assistants/hooli/chat`}
+            name="Example Assistant"
+>>>>>>> Stashed changes
             starterPrompts={[
               {
                 title: 'Display store hours',
