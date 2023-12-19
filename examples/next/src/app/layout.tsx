570e87273418f29e1114d5885f630d7137f1501d
Cole McCracken <colemccracken@gmail.com>
2023-12-19 13:18:21 -0800

    fix external packaging (#55)

--- examples/next/src/app/layout.tsx
+++ examples/next/src/app/layout.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { Providers } from '@definitive-io/openassistants-ui';
-import "@definitive-io/openassistants-ui/globals.css"
+import { Providers } from '@definitive-io/openassistants-react';
+import '@definitive-io/openassistants-react/dist/styles.css';
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -11,8 +11,14 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <Providers defaultTheme="light" disableTransitionOnChange>
-          <main className="flex h-full flex-col flex-1 bg-white">{children}</main>
+        <Providers
+          defaultTheme="light"
+          enableColorScheme
+          disableTransitionOnChange
+        >
+          <main className="flex h-full flex-col flex-1 bg-white">
+            {children}
+          </main>
         </Providers>
       </body>
     </html>
---
+++ packages/openassistants-react/src/components/prompt-form.tsx
@@ -0,0 +1,74 @@
+import * as React from 'react';
+import Textarea from 'react-textarea-autosize';
+
+import { Button } from './ui/button';
+import { Tooltip, TooltipContent, TooltipTrigger } from './ui/tooltip';
+import { IconArrowElbow } from './ui/icons';
+import { useEnterSubmit } from '../lib/hooks/use-enter-submit';
+
+export interface PromptProps {
+  input: string;
+  setInput: (value: string) => void;
+  onSubmit: (value: string) => Promise<void>;
+  isLoading: boolean;
+  placeholder?: string;
+}
+
+export function PromptForm({
+  onSubmit,
+  input,
+  setInput,
+  isLoading,
+}: PromptProps) {
+  const { formRef, onKeyDown } = useEnterSubmit();
+  const inputRef = React.useRef<HTMLTextAreaElement>(null);
+
+  React.useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+
+  return (
+    <form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        if (!input?.trim()) {
+          return;
+        }
+        setInput('');
+        await onSubmit(input);
+      }}
+      ref={formRef}
+    >
+      <div className="relative flex max-h-60 w-full grow flex-col overflow-hidden bg-background px-8 sm:rounded-md sm:border sm:px-12 lg:min-w-600px">
+        <Textarea
+          ref={inputRef}
+          tabIndex={0}
+          onKeyDown={onKeyDown}
+          rows={1}
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Send a message."
+          spellCheck={false}
+          className="min-h-[60px] w-full resize-none bg-transparent px-4 py-[1.3rem] focus-within:outline-none sm:text-sm"
+        />
+        <div className="absolute right-0 top-4 sm:right-4">
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button
+                type="submit"
+                size="icon"
+                disabled={isLoading || input === ''}
+              >
+                <IconArrowElbow />
+                <span className="sr-only">Send message</span>
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>Send message</TooltipContent>
+          </Tooltip>
+        </div>
+      </div>
+    </form>
+  );
+}
