17dde1ed4c9f53586dd5996d2919ff8433676452
Jordan Wu <101218661+jordan-definitive@users.noreply.github.com>
2023-12-20 15:02:57 -0800

    clean up query function to be more extendable
    * remove sqlalchemy engine to make query function more generic

--- packages/openassistants/openassistants/contrib/duckdb_query.py
+++ packages/openassistants/openassistants/contrib/duckdb_query.py
@@ -2,28 +2,27 @@ import os
 from typing import List, Literal
 
 import pandas as pd
-from openassistants.contrib.sqlalchemy_query import QueryFunction
+from openassistants.contrib.sqlalchemy_query import SQLAlchemyFunction
 from openassistants.functions.base import FunctionExecutionDependency
-from sqlalchemy import Engine, create_engine
+from sqlalchemy import create_engine
 
 
-class DuckDBQueryFunction(QueryFunction):
+class DuckDBQueryFunction(SQLAlchemyFunction):
     type: Literal["DuckDBQueryFunction"] = "DuckDBQueryFunction"  # type: ignore
     dataset: str
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._engine = create_engine("duckdb:///:memory:")
+    def __init__(self, **kwargs):
+        super().__init__(engine=create_engine("duckdb:///:memory:"), **kwargs)
 
     async def _execute_sqls(
-        self, engine: Engine, deps: FunctionExecutionDependency
+        self, deps: FunctionExecutionDependency
     ) -> List[pd.DataFrame]:
         # Set workdir to the dataset path
         # Capture the current working directory
         cwd = os.getcwd()
         try:
             os.chdir(self.dataset)
-            res = await super()._execute_sqls(engine, deps)
+            res = await super()._execute_sqls(deps)
         finally:
             # Restore the working directory even if an error occurs
             os.chdir(cwd)
