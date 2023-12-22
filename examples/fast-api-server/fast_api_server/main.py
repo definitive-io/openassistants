e034994b982e8ddb5cecdc843809439294e89aa0
Rick Lamers <ricklamers@gmail.com>
2023-12-22 20:03:40 +0100

    Add OpenAPI support & example.

--- examples/fast-api-server/fast_api_server/main.py
+++ examples/fast-api-server/fast_api_server/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
 from openassistants.core.assistant import Assistant
-from openassistants.functions.crud import PythonCRUD
+from openassistants.functions.crud import OpenAPICRUD, PythonCRUD
 from openassistants_fastapi import RouteAssistants, create_router
 
 from fast_api_server.find_email_by_name_function import find_email_by_name_function
@@ -11,8 +11,14 @@ app = FastAPI()
 # create a library with the custom function
 custom_python_lib = PythonCRUD(functions=[find_email_by_name_function])
 
+openapi_lib = OpenAPICRUD(
+    spec="https://petstore3.swagger.io/api/v3/openapi.json",
+    base_url="https://petstore3.swagger.io/api/v3",
+)
+
+
 hooli_assistant = Assistant(
-    libraries=["piedpiper", custom_python_lib],
+    libraries=["piedpiper", custom_python_lib, openapi_lib],
     scope_description="""Only answer questions about Hooli company related matters.
 You're also allowed to answer questions that refer to anything in the current chat history.""",  # noqa: E501
 )
