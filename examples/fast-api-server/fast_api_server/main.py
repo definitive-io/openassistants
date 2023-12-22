8f90e70ddcf4f1b3e15ab7202c7a7350544feccd
Jordan Wu <101218661+jordan-definitive@users.noreply.github.com>
2023-12-22 13:14:53 -0800

    redis embedding cache

--- examples/fast-api-server/fast_api_server/main.py
+++ examples/fast-api-server/fast_api_server/main.py
@@ -1,12 +1,18 @@
+import os
+
 from fastapi import FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.storage import RedisStore
 from openassistants.core.assistant import Assistant
 from openassistants.functions.crud import OpenAPICRUD, PythonCRUD
+from openassistants.utils.langchain_util import LangChainCachedEmbeddings
 from openassistants_fastapi import RouteAssistants, create_router
 
 from fast_api_server.find_email_by_name_function import find_email_by_name_function
 
 app = FastAPI()
+
 
 # create a library with the custom function
 custom_python_lib = PythonCRUD(functions=[find_email_by_name_function])
@@ -16,11 +22,26 @@ openapi_lib = OpenAPICRUD(
     base_url="https://petstore3.swagger.io/api/v3",
 )
 
+# used redis embedding cache if available
+if (redis_embedding_url := os.getenv("REDIS_URL")) is not None:
+    embedding_cache = RedisStore(
+        redis_url=redis_embedding_url,
+    )
+    entity_embedding_model = LangChainCachedEmbeddings(
+        langchain_underlying_embedder=OpenAIEmbeddings(),
+        langchain_embedding_store=embedding_cache,
+    )
+else:
+    entity_embedding_model = LangChainCachedEmbeddings(
+        langchain_underlying_embedder=OpenAIEmbeddings(),
+    )
+
 
 hooli_assistant = Assistant(
     libraries=["piedpiper", custom_python_lib, openapi_lib],
     scope_description="""Only answer questions about Hooli company related matters.
 You're also allowed to answer questions that refer to anything in the current chat history.""",  # noqa: E501
+    entity_embedding_model=entity_embedding_model,
 )
 
 route_assistants = RouteAssistants(assistants={"hooli": hooli_assistant})
