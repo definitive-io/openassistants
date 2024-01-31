e6b9a34a5cd7080491dc5565589158c2a9455c6a
Rick Lamers <ricklamers@gmail.com>
2024-01-30 16:47:50 -0800

    Remove deprecated endpoint from test

--- examples/fast-api-server/tests/test_app_basic.py
+++ examples/fast-api-server/tests/test_app_basic.py
@@ -7,6 +7,3 @@ client = TestClient(app)
 def test_app_basic():
     response = client.get("/badroute")
     assert response.status_code == 404
-
-    response = client.get("/v1alpha/libraries/hooli/functions")
-    assert response.status_code == 200
